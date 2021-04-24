149f5c0b0c152e26f923ab2eef93e8b39da0b3d0
Nick DiGeronimo <nickdigeronimo@macandcheese.attlocal.net>
2021-04-24 15:50:18 -0700

    first commit nick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import LoginPage from './pages/LoginPage';
+import SearchForTeammatesPage from './pages/SearchForTeammatesPage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <Switch>
+          <Route path="/login/">
+            <LoginPage />
+          </Route>
+          <Route path="/search-for-teammates">
+            <SearchForTeammatesPage />
+          </Route>
+          <Route path="/">
+            home
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import NavBar from '../components/NavBar';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  grid: {
+    marginTop: "25px"
+  }
+}));
+
+export default function LoginPage() {
+  const classes = useStyles();
+    return (
+      <div>
+        <NavBar />
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+        </Grid>
+      </div>
+
+    );
+}
+  
+
+  
