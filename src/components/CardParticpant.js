e967e63c44d73703b9fd33bbfb079d386b480e2a
annevogel13 <annemereldejong@gmail.com>
2021-04-25 17:09:35 +0200

    final card particpant

--- src/components/CardParticpant.js
+++ src/components/CardParticpant.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}  from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,9 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { AirlineSeatLegroomNormal, CenterFocusStrong, EmojiObjects, SignalCellularNoSimOutlined } from '@material-ui/icons';
-import { Collapse } from '@material-ui/core';
-import { pink } from '@material-ui/core/colors';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const useStyles = makeStyles({
   root: {
@@ -18,7 +18,6 @@ const useStyles = makeStyles({
     display: 'inline-block',
     margin: '0 2px',
     transform: 'scale(0.8)',
-    backgroundColor : pink, // can't get it to work
   },
   title: {
     fontSize: 14,
@@ -30,10 +29,10 @@ const useStyles = makeStyles({
 });
 
 // complete array 
-const data = [
+/*const data = [
   {
-    "nickname": "AnneVogel",
-    "interests": ["AI", "Robots"],
+    "nickname": "Anne-Merel",
+    "interests": ["AI", "Robots", "Drones"],
     "skills": ["Javascript", "C++"],
   },
   {
@@ -45,27 +44,56 @@ const data = [
     "nickname": "Shionne",
     "interests": ["Dancing", "dogs"],
     "skills": ["C++", "Fire"],
+  },
+  {
+    "nickname": "Tom",
+    "interests": ["Flutter", "nature"],
+    "skills": ["UI", "UX"],
+  },
+  {
+    "nickname": "Eva",
+    "interests": ["Design", "Comic Books"],
+    "skills": ["3D-development", "UX-experiences"],
   }
-];
-
+]; */
 
 
 export default function SimpleCard({ setInviteList, inviteList }) {
   const classes = useStyles();
+
+  const [data, setData] = React.useState([])
+
+  /*React.useEffect(() => {
+      firebase.database().ref('people').set(data);
+    }) */
+
+  React.useEffect(() => {
+    console.log("get firebase")
+    firebase.database().ref('people').on('value', (snapshot) => {
+      const candidates = snapshot.val();
+      console.log(candidates);
+      setData(candidates); 
+    });
+  }, []);
+
   function addToInvites(name) {
     setInviteList(inviteList.concat([name]))  // adds a name to the listInvites
   }
 
- // return data.map(n => {
-    //let counter = 0 
-    const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+  }, [counter])
+
+  if (data.length == 0) {
+    return <div></div>
+  } else {
     const n = data[counter]
+    const length = Object.entries(data).length;
     const name = n.nickname;
     const skills = n.skills.map(n => n + ", ");
     const interests = n.interests.map(n => n + ", ");
-    useEffect(() => {
-      console.log(counter)
-    }, [counter])
+
     return (
       <Card className={classes.root}>
         <CardContent>
@@ -81,17 +109,16 @@ export default function SimpleCard({ setInviteList, inviteList }) {
                     Javascript, C++, HTML
                   </Typography>
         </CardContent>
-        <CardActions style={{display: "grid", placeContent: "center"}}>
-          <div  style={{display: "grid", columnGap: "10px", gridTemplateColumns: "auto auto auto"}}>
-          <Button size="small" variant="contained" color="primary" button onClick={() => addToInvites(name)}>YES</Button>
-          <Button size="small" variant="contained" color="secondary" button onClick={() => alert("You have permantly deleted a candidate")} >NO</Button>
-          <Button size="small" color="secondary" button onClick={() => setCounter(counter => counter + 1)} >Next person</Button>
+        <CardActions style={{ display: "grid", placeContent: "center" }}>
+          <div style={{ display: "grid", columnGap: "10px", gridTemplateColumns: "auto auto auto" }}>
+            <Button size="small" variant="contained" color="primary" button onClick={() => addToInvites(name)}>YES</Button>
+            <Button size="small" variant="contained" color="secondary" button onClick={() => alert("You have permantly deleted a candidate")} >NO</Button>
+            <Button size="small" color="secondary" button onClick={() => setCounter(counter => (counter + 1) % length)} >Next person</Button>
           </div>
         </CardActions>
       </Card>
     )
-
-  
+  }
 
 
 }
--- src/pages/SearchForTeammatesPage.js
+++ src/pages/SearchForTeammatesPage.js
@@ -9,6 +9,7 @@ import ListInvites from '../components/ListInvites';
 import { pink } from '@material-ui/core/colors';
 import { FreeBreakfast } from '@material-ui/icons';
 import Button from '@material-ui/core/Button';
+import bgImage1 from '../background1.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,7 +40,14 @@ export default function SearchForTeammatesPage() {
   const [inviteList, setInviteList] = React.useState([]);
   const classes = useStyles();
   return (
-    <div>
+    <div style={
+      {
+        height: "125vh",
+        //backgroundImage: "url(" + bgImage1 + ")",
+        backgroundRepeat: 'no-repeat',
+
+      }
+    }>
       <NavBar />
       <Grid container spacing={3} className={classes.grid}>
         <Grid item xs={12}>
@@ -47,20 +55,18 @@ export default function SearchForTeammatesPage() {
             <h1>Search for teammates page</h1>
             <p>
               <h3><u>Description</u></h3>
-               On each card there are three buttons: yes, no, next person. Each of them have seperate function: 
+               On each card there are three buttons: yes, no, next person. Each of them have seperate function:
               <list>
                 <li>Yes : sends a invite to work on your project to the person and it adds </li>
                 <li>No  : means that you don't want to work with this person on your project</li>
                 <li>Next Person : means that you can't make a decision yet, and just want to move on to the next one</li>
               </list>
 
-
-              When you list have listed multiple candidates for your project. There are also some button, where you can manupulate the list itself: 
+              When you list have listed multiple candidates for your project. There are also some button, where you can manupulate the list itself:
               <list>
                 <li>Remove: removes a person from the invite list</li>
                 <li>Send an ... this list : sends a invite to work on your project to all the people on the list</li>
               </list>
-
             </p>
             <div id="cards" className={classes.participantToSwipe}>
 
