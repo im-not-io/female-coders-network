ae6668da9a80fef3f73ae40f3772d805fee6327b
annevogel13 <annemereldejong@gmail.com>
2021-04-25 14:17:21 +0200

    start on backgrounds for homescreen

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import bgImage from '../background.jpg'
-import project from '../womanTyping.jpg'
+import bgImage from '../background.jpg';
+import bgImage1 from '../background1.jpg';
+import project from '../womanTyping.jpg';
+import group from '../group.jpg';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -11,13 +13,13 @@ import { Button } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        paddingTop : "20px",
+       
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        padding : "50px",
+        padding : "15px"
         
 
     },
@@ -31,41 +33,45 @@ export default function HomePage() {
         <div 
             style={
                 {
-                    height: "100vh",
-                    backgroundImage: "url(" + bgImage + ")",
+                    height: "125vh",
+                    backgroundImage: "url(" + bgImage1 + ")",
                     backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
+                   
                 }
             }>
             <NavBar /> {// maybe make transpart for homepage
             }
 
             <div className={classes.root}>
-                <Grid container spacing={3} aligenItems = "center">
+                <Grid container spacing={10}  justify="space-evenly">
                     <Grid item xs={12}>
                         <Paper className={classes.paper}>
                             We help connect women in tech 
                         </Paper>
                     </Grid>
-                    <Grid item xs={4}  justify="center" alignItems="center">
+                    <Grid item xs={4}>
                         <Paper className={classes.paper}>
                             Explore and join tech projects led by women 
                             <div style={
                                 {
                                     backgroundImage: "url(" + project + ")",
                                     padding: "10px",
-                                    height : "50vh",
-                                    width : "50vh"
+                            
                                 }}>
                                 PHOTO
                             </div>
                             <Button color="primary" variant="contained"> Find candidates for your project</Button>
                         </Paper>
                     </Grid>
-                    <Grid item xs={4}  justify="center" alignItems="center">
+                    <Grid item xs={4}>
                         <Paper className={classes.paper}>
                             Connect with other women in tech and take on projects together
-                            <div>
+                            <div style={
+                                {
+                                    backgroundImage : "url("+group+")", 
+                                    padding: "10px", 
+                                }
+                            }>
                                 PHOTO
                             </div>
                             <Button color="primary" variant="contained"> Find Projects to apply to</Button>
